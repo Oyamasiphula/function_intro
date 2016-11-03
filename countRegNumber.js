d19b099ab9fc920cfc778c2c03f0487752c00452
oyamasiphula <oyama@projectcodex.co>
2016-11-03 21:14:23 +0200

    added function that counts and returns the length number of registrations numbers

---
+++ countRegNumber.js
@@ -0,0 +1,7 @@
+var countRegNumber = function(string){
+var regList = string.split(',');
+  console.log(regList.length)
+    return regList.length
+}
+countRegNumber('CA 182736,CY 523519,CJ 812328');
+countRegNumber('CA 42665,AA 12 RT GP');
