062c248458dbd50860fcdc9bdb68955d40f925e2
oyamasiphula <oyama@projectcodex.co>
2016-11-06 23:59:33 +0200

    explained

---
+++ repeat.js
@@ -0,0 +1,21 @@
+// variable named 'Str' assigned with or stores a string value-(Data type);
+var str = 'this is a string';
+// displaying the actual value by loging 'str' to the screen
+console.log(str);
+
+// displaying the actual type of a value by loging 'str' to the screen with the use of a builtin function which has been done for us "by a javascripter developer?" behind the scences
+console.log(typeof str);
+
+// variable named "number" assigned with or stores a Number value-(Data type);
+var number = 100;
+// displaying the actual type of a value by loging "number" to the screen with the use of a builtin function which has been done for us "by a javascripter developer?" behind the scences
+console.log(number);
+
+// displaying the actual type of a value by loging "number" to the screen with the use of a builtin function which has been done for us "by a javascripter developer?" behind the scences
+console.log(typeof number);
+
+// variable named "isBig"[Camel Cased variable name] assigned with or stores a boolean value-(Data type); 
+var isBig = false;
+
+console.log(isBig);
+console.log(typeof isBig);
