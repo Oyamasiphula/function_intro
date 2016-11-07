bdefee9bbd7805ef1598c28a4a0daddeb8063ba7
oyamasiphula <oyama@projectcodex.co>
2016-11-07 22:59:07 +0200

    added function that greets my neighbour(displayed greet)

--- greet.js
+++ greet.js
@@ -1,4 +1,7 @@
+const assert = require('assert');
+
 var greet = function(neighboursName){
-  console.log("Hello : " + neighboursName);
+    console.log("Hello : " + neighboursName);
+  return "Hello : " + neighboursName;
 }
-greet('Songezo');
+assert.equal(greet('Songezo'),"Hello : Songezo");
