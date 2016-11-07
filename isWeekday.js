80a09d1b472ebc9294ec0455e49b27f32d38132b
oyamasiphula <oyama@projectcodex.co>
2016-11-07 23:00:53 +0200

    added function but still confused

--- isWeekday.js
+++ isWeekday.js
@@ -1,11 +1,23 @@
-// var isWeekday = function(){
-// };
-
-const date = new Date('2010-04-01');
-var weekDays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Saturday'];
+const date = new Date('2016-11-07');
+var weekDays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
 
 //return index between 0 and 6
-console.log(date.getDay());
+var isWeekday = function(actualDay){
+  console.log(date.getDay());
+  //look up the week day text string from the Array
+  // console.log(weekDays[date.getDay()]);
+  // console.log(weekDays.indexOf(actualDay));
+  // console.log(weekDays.indexOf(weekDays[date.getDay()]));
+  var isIt = weekDays.indexOf(actualDay) <= weekDays.indexOf(weekDays[date.getDay()]);
+  console.log(isIt);
+}
 
-//look up the week day text string from the Array
-console.log(weekDays[date.getDay()]);
+// isWeekday("Monday");
+// isWeekday("Tuesday");
+// isWeekday("Wednesday");
+// isWeekday("Thursday");
+// isWeekday("Friday");
+//
+// // weekend below
+isWeekday("Saturday");
+// isWeekday("Sunday");
