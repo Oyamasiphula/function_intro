264725e1ed6014f8d808ca5ac7d064813c2eb99f
oyamasiphula <oyama@projectcodex.co>
2016-11-10 20:24:01 +0200

    added a test

--- isWeekday.js
+++ isWeekday.js
@@ -1,20 +1,11 @@
-const date = new Date('2016-11-07');
-var weekDays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+const assert = require('assert');
 
-//return index between 0 and 6
-var isWeekday = function(actualDay){
-  console.log(date.getDay());
-  //look up the week day text string from the Array
-  var checkIfDuringWeek = weekDays.indexOf(actualDay) <= weekDays.indexOf(weekDays[date.getDay()]);
-  console.log(checkIfDuringWeek);
-}
-
-// isWeekday("Monday");
+// assert.equal(isWeekday("Monday"),true);
 // isWeekday("Tuesday");
 // isWeekday("Wednesday");
 // isWeekday("Thursday");
-// isWeekday("Friday");
+// assert.equal(isWeekday("Friday"),true);
 //
 // // weekend below
-isWeekday("Saturday");
+assert.equal(isWeekday("Saturday"),true);
 // isWeekday("Sunday");
