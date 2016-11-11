347d496a304f0e39502bd19440c5ec02573f24ce
oyamasiphula <oyama@projectcodex.co>
2016-11-11 08:55:36 +0200

    added function that checks if the given date data is a weekday

--- isWeekday.js
+++ isWeekday.js
@@ -1,13 +1,12 @@
 const assert = require('assert');
-const date = new Date('2016-11-07');
-
+const date = new Date('2016-11-12');
 var weekDays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
 
 //return index between 0 and 6
 var isWeekday = function(actualDay){
   console.log(date.getDay());
   //look up the week day text string from the Array
-  var isWeekdayValidation = weekDays.indexOf(actualDay) <= weekDays.indexOf(weekDays[date.getDay()]);
+  var isWeekdayValidation = weekDays.indexOf(actualDay) < weekDays.indexOf(weekDays[date.getDay()]);
   console.log(isWeekdayValidation);
   return isWeekdayValidation
 }
