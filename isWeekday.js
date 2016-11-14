96ce83d23e993706bee71d554f7e7110745be727
oyamasiphula <oyama@projectcodex.co>
2016-11-14 11:48:09 +0200

    figured out two ways of cheking if its a weekday or ?

--- isWeekday.js
+++ isWeekday.js
@@ -1,21 +1,35 @@
 const assert = require('assert');
-const date = new Date('2016-11-12');
-var weekDays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
 
-//return index between 0 and 6
-var isWeekday = function(actualDay){
-  console.log(date.getDay());
+var weekDays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+var endOfWeek = weekDays.indexOf('Friday');
+
+var checkIfItsaWeekday = function(day){
+  weekDays.indexOf(day)
   //look up the week day text string from the Array
-  var isWeekdayValidation = weekDays.indexOf(actualDay) < weekDays.indexOf(weekDays[date.getDay()]);
+  var isWeekdayValidation = weekDays.indexOf(day) <= endOfWeek ;
   console.log(isWeekdayValidation);
   return isWeekdayValidation
-}
-// assert.equal(isWeekday("Monday"),true);
-// isWeekday("Tuesday");
-// isWeekday("Wednesday");
-// isWeekday("Thursday");
-// assert.equal(isWeekday("Friday"),true);
-//
+};
+// assert.equal(checkIfItsaWeekday("Monday"),true);
+assert.equal(checkIfItsaWeekday("Tuesday"),true);
+// assert.equal(checkIfItsaWeekday("Wednesday"),true);
+// assert.equal(checkIfItsaWeekday("Thursday"),true);
+// assert.equal(checkIfItsaWeekday("Friday"),true);
 // // weekend below
-assert.equal(isWeekday("Saturday"),true);
-// isWeekday("Sunday");
+assert.equal(checkIfItsaWeekday("Saturday"),false);
+// assert.equal(checkIfItsaWeekday(isWeekday("Sunday"),false);
+
+
+var isWeekday = function(day){
+      var isWeekdayValidation =! day.startsWith('S');
+      return isWeekdayValidation;
+};
+
+// assert.equal(isWeekdayEvaluation("Monday"),true);
+// assert.equal(isWeekdayEvaluation("Tuesday"),true);
+assert.equal(isWeekday("Wednesday"),true);
+// assert.equal(isWeekdayEvaluation("Thursday"),true);
+// assert.equal(isWeekdayEvaluation("Friday"),true);
+// // weekend below
+assert.equal(isWeekday("Saturday"),false);
+// assert.equal(isWeekdayEvaluation(isWeekday("Sunday"),false);
